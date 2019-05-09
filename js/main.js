217e8b2f5dc5d15854c861d1d4ad4492634b960d
Joey <joeylau1998@gmail.com>
2019-05-09 18:04:33 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -197,8 +197,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	const lastVisitedList = JSON.parse(localStorage.getItem('lastList'));
 	// Show all list from Localstorage
-	console.log(data.data);
-	if(data.data) {
+	if(data) {
 		showLists(data, listContainer, taskContainer);
 
 		refreshList(data, lastVisitedList);
